ebb0e3c2f7ec1f25f8517b468d8269e8c864f2a4
Salman Ansari <salmanansari@Salmans-MBP.fios-router.home>
2021-06-10 22:28:12 -0400

    added city name , temprature,minand low

--- index.js
+++ index.js
@@ -2,20 +2,54 @@ const searchT = document.querySelector('#search-text');
 const searchB = document.querySelector('button');
 const domain = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const api_key = 'da71d063bfd411d53a8841266cd91c67';
+const containerOne = document.querySelector('#one');
+const containerTwo = document.querySelector('#two');
+const containerThree = document.querySelector('#three');
+const containerFour = document.querySelector('#four');
+const allContainers = (containerOne,containerTwo,containerThree,containerFour);
+
+const searchRemove = ()=>{
+while(allContainers.lastChild){
+    (allContainers).removeChild(allContainers.lastChild)
+}
+}
+
 const searchCity = async ()=>{
-    
+    searchRemove();
     try{
         const city = searchT.value;
         const url = await axios.get(`${domain}${city}&appid=${api_key}`);
         
-        const finalR = url.data.main.temp;
-        console.log(finalR);
-        const containerOne = document.querySelector('#one');
+        
+        
+        // City Name
+        const cityName = url.data.name;
+        const cityN = document.createElement('h1');
+        cityN.innerText = `${cityName}`;
+        containerOne.appendChild(cityN);
 
-const tempResult = document.createElement('h2');
- tempResult.innerText = `Temprature: ${finalR}`;
+        //Temrature
+        const temprature = url.data.main.temp;
+        const tempResult = document.createElement('h2');
+        tempResult.innerText = `${Math.floor(parseInt(temprature - 273.15)*(1.8)+32)}℉ `;
+        containerOne.appendChild(tempResult);
 
- containerOne.appendChild(tempResult);
+        // Min Temp
+        const minTemp = url.data.main.temp_min;
+        const minT = document.createElement('h3');
+        minT.innerText= `Low: ${Math.floor(parseInt(temprature - 273.15)*(1.8)+32)}℉`;
+        containerOne.append(minT);
+
+        //High Temp
+        const highTemp = url.data.main.temp_min;
+        const minH = document.createElement('h3');
+        minH.innerText= `High: ${Math.floor(parseInt(temprature - 273.15)*(1.8)+32)}℉`;
+        containerOne.append(minH);
+        
+
+        
+    
+        
 
     }
     catch (error){
@@ -23,9 +57,19 @@ const tempResult = document.createElement('h2');
     }
 
 }
+
+// Display Funtion
+// const display = (city) =>{
+    
+    
+
+// }
+
+
+
 searchB.addEventListener('click', (e) => {
     e.preventDefault();
     searchCity();
 })
 
-
+searchRemove();
